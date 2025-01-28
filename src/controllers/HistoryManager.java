9317612cb6fe3d2552bb40344a06ba0acb4e3c5a
Denis Nagovitsyn <disochkin@gmail.com>
2025-01-28 22:11:25 +0500

    1. Убрал ограничение на длину списка для хранения истории просмотра задач. 2. Повторно просмотренные задачи не сохраняются в истории просмотра. 3. Обновил юнит-тесты.

--- src/controllers/HistoryManager.java
+++ src/controllers/HistoryManager.java
@@ -2,10 +2,15 @@ package controllers;
 
 import model.Task;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface HistoryManager {
     void updateHistory(Task viewedTask);
+    void updateHistory(List<Task> viewedTaskList);
 
-    ArrayList<Task> getHistory();
+    void remove(int id);
+
+    void remove(List<Integer> listOfId);
+
+    List<String> getHistory();
 }
